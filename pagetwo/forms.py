a04aaec6ec1ade9b0068fa75f124c5fa67bfea41
gajendra <gajendrambr8@gmail.com>
2019-10-10 16:34:53 -0700

    final

--- pagetwo/forms.py
+++ pagetwo/forms.py
@@ -48,6 +48,21 @@ class userloginform(forms.Form):
     #         'placeholder': 'myCustomPlaceholder'})
 
 
+# class UserForm(forms.ModelForm):
+#     password=forms.CharField(widget=forms.PasswordInput())
+#     confirm_password=forms.CharField(widget=forms.PasswordInput())
+#
+#     class Meta:
+#         model = ebregister
+#         fields='__all__'
+#
+#     def clean(self):
+#         cleaned_data = super(UserForm, self).clean()
+#         password = cleaned_data.get("password")
+#         confirm_password = cleaned_data.get("confirm_password")
+#
+#         if password != confirm_password:
+#             raise forms.ValidationError("password and confirm_password does not match")
 
 
 
