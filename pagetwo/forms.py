0a19c7c033cf8c2a8a18f5ea3a87b1543acf865a
gajendra <gajendrambr8@gmail.com>
2019-10-10 16:22:41 -0700

    second

--- pagetwo/forms.py
+++ pagetwo/forms.py
@@ -96,10 +96,3 @@ class CustomUserCreationForm(forms.Form):
 
         return password2
 
-    def save(self, commit=True):
-        user = User.objects.create_user(
-            self.cleaned_data['username'],
-            self.cleaned_data['email'],
-            self.cleaned_data['password1']
-        )
-        return user
