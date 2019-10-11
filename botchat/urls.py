55dc322c7bfd6ebf7f56048720ef7d12467b1352
gajendra <gajendrambr8@gmail.com>
2019-10-10 18:02:59 -0700

    final1

--- botchat/urls.py
+++ botchat/urls.py
@@ -3,7 +3,7 @@ from botchat.views import ChatterBotAppView, ChatterBotApiView
 
 
 urlpatterns=[
-    url(r'^chaterbot$', ChatterBotAppView.as_view(), name='chat'),
+    url(r'^chater$', ChatterBotAppView.as_view(), name='chat'),
     url(r'^api/chatterbot/', ChatterBotApiView.as_view(), name='chatterbot'),
 
 ]
